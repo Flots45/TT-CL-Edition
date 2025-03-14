49be9139168ffee9ac12df6941e897937e4c4e63
Flots45 <xduckingly@gmail.com>
2025-03-13 19:19:02 -0700

    Update DistributedCashbotBossAI.py

--- toontown/suit/DistributedCashbotBossAI.py
+++ toontown/suit/DistributedCashbotBossAI.py
@@ -634,7 +634,7 @@ class DistributedCashbotBossAI(DistributedBossCogAI.DistributedBossCogAI, FSM.FS
         self.goonMovementTime = globalClock.getFrameTime()
         if side == None:
             if not self.wantOpeningModifications:
-                side = random.choice(['EmergeB', 'EmergeB'])
+                side = random.choice(['EmergeA', 'EmergeA'])
             else:
                 for t in self.involvedToons:
                     avId = t
